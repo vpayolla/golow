642d7122f70108ccbcb8e8d87c2ebcbfac3b305d
Mistah Rollo <vpay.rollo@gmail.com>
2023-11-14 19:19:24 +0000

    added a sleep

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, world")
+
+	// time.Sleep(1000 * time.Millisecond)
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Goodbye, world")
 }
